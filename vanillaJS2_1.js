5fee36820ae8817e3ba84c15f063b2850f22bc38
YunKukPark <53929065+YunKukPark@users.noreply.github.com>
2020-01-02 21:34:29 +0900

    Create vanillaJS2_1.js

---
+++ vanillaJS2_1.js
@@ -0,0 +1,11 @@
+function sayHello(potato, age){
+  console.log("Hello!" , potato, "you have " ,age, "years of age.");
+}
+
+function sayHelloAlert(name, age){
+  alert("Hello! " + name + " your age is " +  age)
+}
+
+sayHello("Yunkuk" , 15);
+
+sayHelloAlert("niko", 22);
