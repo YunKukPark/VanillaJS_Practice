36fdc634cf441b72bd9b9eb626adfb75ff339802
YunKukPark <53929065+YunKukPark@users.noreply.github.com>
2020-01-03 20:45:43 +0900

    Add files via upload

---
+++ ModifyingTheDom.js
@@ -0,0 +1,6 @@
+// const title = document.getElementById("title");
+const title = document.querySelector("#title");
+title.innerHTML = "Hi myname is dd";
+title.style.color = 'red';
+// console.dir(document);
+document.title = "i Own you now"
